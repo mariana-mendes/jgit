6c077c068745147e0f492453be6d3b18e011739e
mariana-mendes <mariana.silva@ccc.ufcg.edu.br>
2017-10-17 15:43:10 -0300

    aa tem aula

---
+++ org.eclipse.jgit/src/main/java/org/eclipse/jgit/org/eclipse/jgit/Testing.java
@@ -0,0 +1,9 @@
+package org.eclipse.jgit.org.eclipse.jgit;
+
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+
+public class Testing {
+	
+	FileRepositoryBuilder builder = new FileRepositoryBuilder();
+
+}
