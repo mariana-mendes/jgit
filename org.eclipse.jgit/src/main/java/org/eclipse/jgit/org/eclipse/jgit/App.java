6fd334fa5fa4d87bfe084f93ecbd06d8826d8deb
mariana-mendes <mariana.silva@ccc.ufcg.edu.br>
2017-10-20 11:36:45 -0300

    conseguindo listar commits da branch mastergit config user.email mariana.silva@ccc.ufcg.edu.brgit config user.email mariana.silva@ccc.ufcg.edu.br! <3

--- org.eclipse.jgit/src/main/java/org/eclipse/jgit/org/eclipse/jgit/App.java
+++ org.eclipse.jgit/src/main/java/org/eclipse/jgit/org/eclipse/jgit/App.java
@@ -4,10 +4,19 @@ package org.eclipse.jgit.org.eclipse.jgit;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
+	
+	static Testing t = new Testing();
+	
     public static void main( String[] args )
     {
+    	t.criaRepo();
+    	try {
+			t.revWalk();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         System.out.println( "Hello World!" );
     }
 }
--- org.eclipse.jgit/src/main/java/org/eclipse/jgit/org/eclipse/jgit/Testing.java
+++ org.eclipse.jgit/src/main/java/org/eclipse/jgit/org/eclipse/jgit/Testing.java
@@ -1,9 +1,52 @@
 package org.eclipse.jgit.org.eclipse.jgit;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.attributes.AttributesNodeProvider;
+import org.eclipse.jgit.lib.ObjectDatabase;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.RefDatabase;
+import org.eclipse.jgit.lib.ReflogReader;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.RepositoryBuilder;
+import org.eclipse.jgit.lib.StoredConfig;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
 public class Testing {
 	
-	FileRepositoryBuilder builder = new FileRepositoryBuilder();
+	Repository repository = null;
 
-}
+	public void criaRepo() {
+		try {
+			 repository = new FileRepositoryBuilder().findGitDir(new File("/home/marianams/projeto_si")).build();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(repository.getGlobalListenerList());
+
+	};
+	
+	
+	  static void revWalk() throws Exception {
+	        Repository repository;
+	        RevWalk revWalk;
+	        ObjectId commitId;
+	        Git git = Git.open(new File("/home/marianams/projeto_si")); 
+	        repository = git.getRepository();
+	    
+	        revWalk = new RevWalk(repository);
+	        commitId = repository.resolve("refs/heads/master");
+	       
+
+	        revWalk.markStart( revWalk.parseCommit( commitId ) );
+	        for( RevCommit commit : revWalk ) {
+	          System.out.println( commit.getFullMessage() );
+	        }
+	    }
+};
