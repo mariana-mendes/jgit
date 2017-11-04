8fef5410c5dde975464d8b76226113b65bf659ff
mariana-mendes <mariana.silva@ccc.ufcg.edu.br>
2017-11-04 14:12:47 -0300

    BlAAAAame

--- src/main/java/org/eclipse/jgit/org/eclipse/jgit/ListGit.java
+++ src/main/java/org/eclipse/jgit/org/eclipse/jgit/ListGit.java
@@ -13,13 +13,14 @@ public class ListGit {
 	static RepositorioExistente t = new RepositorioExistente();
 	
     public static void main( String[] args )  {
-    	String directory = "/home/marianams/HotelGotemburgo";
+    	String directory = "/home/mariana/homemade-dynamite";
     	String branch = "refs/heads/master";
     
     	
     	try {
     		Repository repository = t.openRepository(directory);
     		t.revWalk(repository, branch);
+    		t.gitBlame(repository);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- src/main/java/org/eclipse/jgit/org/eclipse/jgit/RepositorioExistente.java
+++ src/main/java/org/eclipse/jgit/org/eclipse/jgit/RepositorioExistente.java
@@ -3,7 +3,13 @@ package org.eclipse.jgit.org.eclipse.jgit;
 import java.io.File;
 import java.io.IOException;
 
+import org.eclipse.jgit.api.BlameCommand;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.blame.BlameResult;
+import org.eclipse.jgit.errors.AmbiguousObjectException;
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.RevisionSyntaxException;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
@@ -26,7 +32,6 @@ public class RepositorioExistente {
 		}
 	};
 	
-	
 	  public void revWalk(Repository repository, String branch) throws Exception {
 	        RevWalk revWalk;
 	        ObjectId commitId;
@@ -37,5 +42,25 @@ public class RepositorioExistente {
 	        for( RevCommit commit : revWalk ) {
 	          System.out.println( "Author: "+commit.getAuthorIdent().getEmailAddress() + ". Message: " + commit.getFullMessage() );
 	        }
-	    }
+	    } 
+	  
+	  
+	  public void gitBlame(Repository repository) throws RevisionSyntaxException, AmbiguousObjectException, IncorrectObjectTypeException, IOException, GitAPIException {
+		  final int teste = 3;
+		  
+		  BlameCommand blame = new BlameCommand(repository);
+		  ObjectId commitID = repository.resolve("HEAD");
+		  blame.setStartCommit(commitID);
+		  blame.setFilePath("src/album/Album.java");
+		  BlameResult blameResult = blame.call();
+		  System.out.println(blameResult);
+		  System.out.println(blame.getRepository());
+		
+		  for (int i = 0; i < teste; i++) {
+			  RevCommit commit = blameResult.getSourceCommit(i);
+			  System.out.println("Line: " + i + ": " + commit.getAuthorIdent().getName());
+		}
+		  
+		  
+	  }
 };
